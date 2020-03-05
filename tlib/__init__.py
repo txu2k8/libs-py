3036f50f7c9f89fd2549fe8028cca8e73c06b81b
tao.xu <tao.xu2008@outlook.com>
2020-03-05 10:05:44 +0800

    v1.2.9

--- tlib/__init__.py
+++ tlib/__init__.py
@@ -4,4 +4,12 @@
 # @Author  : Tao.Xu
 # @Email   : tao.xu2008@outlook.com
 
-"""test libs"""
+"""
+Some own/observed great lib/ideas,common useful python libs.
+"""
+
+import sys
+from tlib import version
+
+if sys.version_info < (2, 6):
+    raise ImportError('tlib needs to be run on python 2.6 and above.')
